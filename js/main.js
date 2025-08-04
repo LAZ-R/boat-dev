6343a24fad4f074451d5622dc4c906bfdf817fe1
Clément Duquenoy <cduquenoy@asi.fr>
2025-08-04 18:15:34 +0200

    train commit

--- js/main.js
+++ js/main.js
@@ -14,32 +14,29 @@ import {
 import { getRandomIntegerBetween } from "./utils/math.utils.js";
 import { setHTMLTitle, logAppInfos } from "./utils/UTILS.js";
 // VARIABLES //////////////////////////////////////////////////////////////////////////////////////
+// UI -----------------------
 const HEADER = document.getElementById('header');
 const MAIN = document.getElementById('main');
-
+// CONST --------------------
 const LETTERS = ['A','B','C','D','E','F','G','H','I','J','K'];
 const NUMBERS = [1,2,3,4,5,6,7,8,9,10,11];
+const COLLIDABLES = ['rock-1','rock-2','rock-3'];
+const BONUSES = ['bonus-1','bonus-2','bonus-3'];
+const MIN_LINES_BETWEEN_BONUS = 10;
+// Current game -------------
+let isTouchScreen = false;
 
-let fullPercentage = 10;
-let bgPosition = 1;
+let collidablePercentage = 10;
+let bonusPercentage = 40;
 
-let CURRENT_PLAYER_COLUMN = 6;
 let currentScore = 0;
+let currentBgPosition = 1;
+let currentPlayerColumn = 6;
+let currentLinesBeforeBonus = MIN_LINES_BETWEEN_BONUS;
+let currentBonus = null;
 let isCrashed = false;
-let isTouch = false;
 
 let defilement = null;
-
-const loremIpsum = `
-<p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae imperdiet est, nec fringilla sem. Donec lectus ex, dignissim non vestibulum et, consectetur vitae erat. Phasellus et tortor nec risus semper rutrum porta vitae odio. Sed at mollis turpis. Integer pulvinar lobortis mi, lobortis dictum mi commodo in. Fusce augue metus, scelerisque ac molestie et, ultricies vitae purus. Sed at pharetra augue, at eleifend metus.
-  <br>
-  Aenean lorem odio, fringilla et hendrerit nec, hendrerit et felis. Maecenas interdum porta tellus, nec ultrices lectus commodo sit amet. Vivamus a turpis metus. Nunc mattis velit non enim aliquam volutpat. Aenean eleifend risus sed augue facilisis, congue pretium ipsum consequat. Suspendisse eu nulla placerat, mattis nisl eu, feugiat nisl. Nulla felis risus, aliquet eu posuere eu, imperdiet sed velit. Sed ac tellus cursus, mollis sapien id, dapibus nisl. Proin mauris nisi, blandit quis efficitur vulputate, venenatis ut nisl. Phasellus viverra quis nunc iaculis rutrum. Duis at tortor convallis, eleifend nunc venenatis, dapibus neque. Duis imperdiet mollis lacus mattis bibendum. Curabitur sit amet tellus gravida, viverra libero id, tincidunt orci.
-</p>
-`;
-
-const options = `<option value="option 1" selected>option 1</option>
-    <option value="option 2">option 2</option>`;
 
 // FUNCTIONS //////////////////////////////////////////////////////////////////////////////////////
 
@@ -47,7 +44,8 @@ const options = `<option value="option 1" selected>option 1</option>
 const onStartClick = () => {
   document.getElementById('startContainer').classList.add('hidden')
   defilement = setInterval(() => {
-    const newLines = updateCanevasLines(lines);
+    currentLinesBeforeBonus -= 1;
+    const newLines = updateCanevasLines(getCurrentCanevasLines());
     //console.log(newLines)
     setCanevas(newLines);
     checkCollision();
@@ -59,13 +57,102 @@ const onStartClick = () => {
 };
 window.onStartClick = onStartClick;
 
+const onUseBonusClick = () => {
+  if (currentBonus !== null) {
+    //console.log(`USING ${currentBonus}`);
+    const bonusDisplay = document.getElementById('currentBonusDisplay');
+    
+    switch (currentBonus) {
+      case 'bonus-1':
+        bonus_clearCurrentGrid();
+        break;
+      case 'bonus-2':
+        bonus_clearCurrentGrid();
+        break;
+      case 'bonus-3':
+        bonus_clearCurrentGrid();
+        break;
+      default:
+        break;
+    }
+    currentBonus = null;
+    bonusDisplay.classList = [];
+    bonusDisplay.classList.add('current-bonus');
+  }
+};
+window.onUseBonusClick = onUseBonusClick;
+
+document.onkeydown = function (e) {
+  if (e.key === "ArrowLeft") {
+    movePlayer('left');
+  } else if (e.key === "ArrowRight") {
+    movePlayer('right');
+  }
+};
+
+const onUserTouchStart = (direction) => {
+  isTouchScreen = true;
+  movePlayer(direction);
+}
+window.onUserTouchStart = onUserTouchStart;
+
+const onClickStart = (direction) => {
+  if (!isTouchScreen) {
+    movePlayer(direction);
+  }
+}
+window.onClickStart = onClickStart;
+
 // DATA #######################################################################
+const getRandomCollidableClass = () => {
+  return COLLIDABLES[getRandomIntegerBetween(0, COLLIDABLES.length -1)];
+}
+
+const bonus_clearCurrentGrid = () => {
+  const canevasLinesDOM = document.getElementsByClassName('canevas-line');
+  const canevasLines = [];
+  //console.log(canevasLinesDOM);
+  for (let line of canevasLinesDOM) {
+    const lineCells = [];
+    const cells = line.children;
+    for (let cell of cells) {
+      const isCollidable = cell.classList.contains('collidable');
+  
+      let cellObj = {
+        isCollidable: false,
+        collidableClass: '',
+        isBonus: false,
+        bonusClass: '',
+      };
+
+      if (isCollidable) {
+        let letter = cell.getAttribute('id')[0];
+        if (letter === 'A' || letter === 'K') {
+          cellObj.isCollidable = true;
+          cellObj.collidableClass = cell.classList[cell.classList.length - 1];
+        }
+      }
+      lineCells.push(cellObj);
+    }
+    canevasLines.push(lineCells);
+  }
+  setCanevas(canevasLines);
+}
+
+const getRandomBonusClass = () => {
+  return BONUSES[getRandomIntegerBetween(0, BONUSES.length -1)];
+}
+
 const getBaseCanevasLine = (lineNumber, isLineEmpty = false) => {
   let str = '';
   for (let letter of LETTERS) {
     const isFirstOrLast = letter === 'A' || letter === 'K';
-    const isFull = getRandomIntegerBetween(1, 100) <= fullPercentage;
-    str += `<div id="${letter}${lineNumber}" class="canevas-cell ${isFirstOrLast ? 'full' : ''} ${isLineEmpty ? '' : isFull ? 'full' : ''}"></div>`;
+    if (isFirstOrLast) {
+      str += `<div id="${letter}${lineNumber}" class="canevas-cell collidable ${getRandomCollidableClass()}"></div>`;
+    } else {
+      const isCollidable = getRandomIntegerBetween(1, 100) <= collidablePercentage;
+      str += `<div id="${letter}${lineNumber}" class="canevas-cell ${isLineEmpty ? '' : isCollidable ? `collidable ${getRandomCollidableClass()}` : ''}"></div>`;
+    }
   }
   return str;
 }
@@ -88,8 +175,24 @@ const getCurrentCanevasLines = () => {
     const lineCells = [];
     const cells = line.children;
     for (let cell of cells) {
-      //console.log(cell.classList.length);
-      let cellObj = {full: cell.classList.contains('full')};
+      const isCollidable = cell.classList.contains('collidable');
+      const isBonus = cell.classList.contains('bonus');
+
+      let cellObj = {
+        isCollidable: false,
+        collidableClass: '',
+        isBonus: false,
+        bonusClass: '',
+      };
+
+      if (isCollidable) {
+        cellObj.isCollidable = true;
+        cellObj.collidableClass = cell.classList[cell.classList.length - 1];
+      } else if (isBonus) {
+        cellObj.isBonus = true;
+        cellObj.bonusClass = cell.classList[cell.classList.length - 1];
+      }
+
       lineCells.push(cellObj);
     }
     canevasLines.push(lineCells);
@@ -102,7 +205,17 @@ const getCanevasLineByArray = (lineNumber, lineCells) => {
   for (let index = 0; index < lineCells.length; index++) {
     const letter = LETTERS[index];
     const cell = lineCells[index];
-    str += `<div id="${letter}${lineNumber}" class="canevas-cell ${cell.full ? 'full' : ''}"></div>`;
+
+    const isCollidable = cell.isCollidable;
+    const isBonus = cell.isBonus;
+
+    if (isCollidable) {
+      str += `<div id="${letter}${lineNumber}" class="canevas-cell collidable ${cell.collidableClass}"></div>`;
+    } else if (isBonus) {
+      str += `<div id="${letter}${lineNumber}" class="canevas-cell bonus ${cell.bonusClass}"></div>`;
+    } else {
+      str += `<div id="${letter}${lineNumber}" class="canevas-cell"></div>`;
+    }
   }
   return str;
 }
@@ -119,65 +232,78 @@ const getCanevasByLines = (canevasLines) => {
   return str;
 }
 
-const updateCanevasLines = (canevasLines) => {
-  canevasLines.pop();
+const getNewLineCells = () => {
+  let bonusProbability = 10;
   const newLineCells = [];
   for (let letter of LETTERS) {
-    const isFull = letter === 'A' || letter === 'K' || getRandomIntegerBetween(1, 100) <= fullPercentage;
-    let cellObj = {full: isFull};
+    let cellObj = {
+      isCollidable: false,
+      collidableClass: '',
+      isBonus: false,
+      bonusClass: '',
+    };
+
+    const isFirstOrLast = letter === 'A' || letter === 'K';
+    
+    if (isFirstOrLast) {
+      cellObj.isCollidable = true;
+      cellObj.collidableClass = getRandomCollidableClass();
+    } else {
+      const isCollidable = getRandomIntegerBetween(1, 100) <= collidablePercentage;
+      if (isCollidable) {
+        cellObj.isCollidable = true;
+        cellObj.collidableClass = getRandomCollidableClass();
+      } else {
+        const isBonus = getRandomIntegerBetween(1, 100) <= bonusProbability;
+        if (isBonus && currentLinesBeforeBonus === 0) {
+          currentLinesBeforeBonus = MIN_LINES_BETWEEN_BONUS;
+          cellObj.isBonus = true;
+          cellObj.bonusClass = getRandomBonusClass();
+        }
+        bonusProbability += 10;
+      }
+    }
     newLineCells.push(cellObj);
   }
+  return newLineCells;
+}
+
+const updateCanevasLines = (canevasLines) => {
+  canevasLines.pop();
+  const newLineCells = getNewLineCells();
   canevasLines.unshift(newLineCells);
   return canevasLines;
 }
 
 const setCanevas = (canevasLines) => {
-  const canevasContainer = document.getElementById('canevasContainer');
-  canevasContainer.innerHTML = '';
-  bgPosition += 1;
-  canevasContainer.style = `background-position-y: calc(${bgPosition} * var(--cell-size));`;
-  canevasContainer.innerHTML = getCanevasByLines(canevasLines);
+  const canevasLinesContainer = document.getElementById('canevasLinesContainer');
+  const canevasBg = document.getElementById('canevasBg');
+  canevasLinesContainer.innerHTML = '';
+  currentBgPosition += 1;
+  canevasBg.style = `background-position-y: calc(${currentBgPosition} * var(--cell-size));`;
+  canevasLinesContainer.innerHTML = getCanevasByLines(canevasLines);
 }
 
 const movePlayer = (direction) => {
   if (!isCrashed) {
     const player = document.getElementById('player');
     if (direction === 'left') {
-      CURRENT_PLAYER_COLUMN -= 1;
+      currentPlayerColumn -= 1;
     } else if (direction === 'right') {
-      CURRENT_PLAYER_COLUMN += 1;
+      currentPlayerColumn += 1;
     }
-    player.style = `left: calc(var(--button-size) + (${CURRENT_PLAYER_COLUMN - 1} * var(--cell-size)));`;
+    player.style = `left: calc(var(--button-size) + (${currentPlayerColumn - 1} * var(--cell-size)));`;
     checkCollision();
   }
 }
 
-document.onkeydown = function (e) {
-  if (e.key === "ArrowLeft") {
-    movePlayer('left');
-  } else if (e.key === "ArrowRight") {
-    movePlayer('right');
-  }
-};
 
-const onUserTouchStart = (direction) => {
-  isTouch = true;
-  movePlayer(direction);
-}
-window.onUserTouchStart = onUserTouchStart;
-
-const onClickStart = (direction) => {
-  if (!isTouch) {
-    movePlayer(direction);
-  }
-}
-window.onClickStart = onClickStart;
 
 const checkCollision = () => {
-  const currentPlayerCell = `${LETTERS[CURRENT_PLAYER_COLUMN - 1]}9`;
-  console.log(currentPlayerCell);
+  const currentPlayerCell = `${LETTERS[currentPlayerColumn - 1]}9`;
+  //console.log(currentPlayerCell);
   const cell = document.getElementById(currentPlayerCell);
-  if (cell.classList.contains('full')) {
+  if (cell.classList.contains('collidable')) {
     isCrashed = true;
     clearInterval(defilement);
     const player = document.getElementById('player');
@@ -192,56 +318,67 @@ const checkCollision = () => {
       document.getElementById('gameOverContainer').classList.remove('hidden');
     }, 500);
   }
+  if (cell.classList.contains('bonus')) {
+    const bonusClass = cell.classList[cell.classList.length - 1];
+    currentBonus = bonusClass;
+    cell.classList.remove('bonus');
+    console.log(`CURRENT BONUS: ${currentBonus}`);
+    const bonusDisplay = document.getElementById('currentBonusDisplay');
+    bonusDisplay.classList = [];
+    bonusDisplay.classList.add('current-bonus');
+    bonusDisplay.classList.add(currentBonus);
+  }
+}
+
+const setStep = (stepNumber) => {
+  const canevasContainer = document.getElementById('canevasContainer');
+  if (!canevasContainer.classList.contains(`step-${stepNumber}`)) {
+    canevasContainer.classList.remove(`step-${stepNumber - 1}`);
+    canevasContainer.classList.add(`step-${stepNumber}`);
+  }
 }
 
 const updateScore = () => {
   currentScore += 50;
   document.getElementById('currentScore').innerHTML = `${currentScore} pts`;
 
-  const canevasContainer = document.getElementById('canevasContainer');
-
-  if (currentScore <= 1000) {
-    fullPercentage = 10;
+  if (currentScore < 1000) {
+    collidablePercentage = 10;
   }
-  if (currentScore > 1000 && currentScore <= 2500) {
-    fullPercentage = 12;
+  if (currentScore >= 1000 && currentScore < 2500) {
+    collidablePercentage = 12;
   }
-  if (currentScore > 2500 && currentScore <= 5000) {
-    fullPercentage = 15;
+  if (currentScore >= 2500 && currentScore < 5000) {
+    collidablePercentage = 15;
+    setStep(1);
   }
-  if (currentScore > 5000 && currentScore <= 7500) {
-    if (!canevasContainer.classList.contains('step1')) {
-      canevasContainer.classList.add('step1')
-    }
-    fullPercentage = 18;
+  if (currentScore >= 5000 && currentScore < 7500) {
+    collidablePercentage = 18;
+    setStep(2);
   }
-  if (currentScore > 7500 && currentScore <= 10000) {
-    if (!canevasContainer.classList.contains('step2')) {
-      canevasContainer.classList.remove('step1');
-      canevasContainer.classList.add('step2');
-    }
-    fullPercentage = 20;
+  if (currentScore >= 7500 && currentScore < 10000) {
+    collidablePercentage = 20;
+    setStep(3);
   }
-  if (currentScore > 10000 && currentScore <= 15000) {
-    if (!canevasContainer.classList.contains('step3')) {
-      canevasContainer.classList.remove('step2');
-      canevasContainer.classList.add('step3');
-    }
-    fullPercentage = 22;
+  if (currentScore >= 10000 && currentScore < 12500) {
+    setStep(4);
+    collidablePercentage = 21;
   }
-  if (currentScore > 15000 && currentScore <= 20000) {
-    if (!canevasContainer.classList.contains('step4')) {
-      canevasContainer.classList.remove('step3');
-      canevasContainer.classList.add('step4');
-    }
-    fullPercentage = 24;
+  if (currentScore >= 12500 && currentScore < 15000) {
+    collidablePercentage = 22;
+    setStep(5);
   }
-  if (currentScore > 20000) {
-    if (!canevasContainer.classList.contains('step5')) {
-      canevasContainer.classList.remove('step4');
-      canevasContainer.classList.add('step5');
-    }
-    fullPercentage = 26;
+  if (currentScore >= 15000 && currentScore < 17500) {
+    collidablePercentage = 23;
+    setStep(6);
+  }
+  if (currentScore >= 17500 && currentScore < 20000) {
+    collidablePercentage = 24;
+    setStep(7);
+  }
+  if (currentScore >= 20000) {
+    collidablePercentage = 25;
+    setStep(8);
   }
 }
 // IHM RENDER #################################################################
@@ -260,8 +397,14 @@ MAIN.innerHTML += `
 <button class="move-button" onmousedown="onClickStart('left')" ontouchstart="onUserTouchStart('left')">
   <img src="./medias/images/left2.png" />
 </button>
-<div id="canevasContainer" class="canevas-container">
-  ${getBaseCanevas()}
+<div id="canevasContainer" class="canevas-container step-0">
+  <div id="canevasBg" class="canevas-bg"></div>
+  <div id="canevasLinesContainer" class="canevas-lines">
+    ${getBaseCanevas()}
+  </div>
+</div>
+<div class="use-bonus-container" onclick="onUseBonusClick()">
+  <div id="currentBonusDisplay" class="current-bonus"></div>
 </div>
 <div id="player" class="player"></div>
 <div id="startContainer" class="start-container">
@@ -276,5 +419,3 @@ MAIN.innerHTML += `
   <img src="./medias/images/right2.png" />
 </button>
 `;
-const lines = getCurrentCanevasLines();
-//console.log(lines);
